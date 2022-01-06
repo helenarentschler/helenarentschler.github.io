64acaaa8db7674b662ef3132586270695c081d7e
Helena Rentschler <helrentschler@gmail.com>
2022-01-06 13:09:03 -0300

    add files
    js, html, css files added

---
+++ scripts/main.js
@@ -0,0 +1,34 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/helena.jpeg'){
+        myImage.setAttribute('src', 'images/helena2.JPG');
+    } else {
+        myImage.setAttribute('src', 'images/helena.jpeg');
+    }
+}
+
+let myButton = document.querySelector('button');
+let myMessage = document.querySelector('.top-message');
+
+function setUserName(){
+    let myName = prompt('Escreva o seu nome:');
+    if (!myName){
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myMessage.textContent = 'Seja bem vindo(a), ' + myName + '!';
+    }
+}
+
+if (!localStorage.getItem('name')){
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myMessage.textContent = 'Seja bem vindo(a), ' + storedName + '!';
+}
+
+myButton.onclick = function () {
+    setUserName ();
+}
